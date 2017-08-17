b2e7a600f837f1024f6ddb71039ad60e3c56bf07
Mogria <m0gr14@gmail.com>
2017-08-17 15:32:54 +0200

    add nosetests to nix dependencies

--- default.nix
+++ default.nix
@@ -1,8 +1,10 @@
 let 
   pkgs = import <nixpkgs> { };
   stdenv = pkgs.stdenv;
+  python = pkgs.python36;
+  pythonPackages = pkgs.python36Packages;
 in
   stdenv.mkDerivation rec {
     name = "knockd-env";
-    buildInputs = with pkgs; [ python36 ];
+    buildInputs = [ python pythonPackages.pycrypto pythonPackages.nose ];
   }
