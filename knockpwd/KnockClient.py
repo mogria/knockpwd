074cf471726b2b8ddcbe597d7eaec8a105caadd6
Mogria <m0gr14@gmail.com>
2017-08-20 20:36:14 +0200

    fix some pyline warnings

--- knockpwd/KnockClient.py
+++ knockpwd/KnockClient.py
@@ -1,9 +1,10 @@
 import socket
-import struct
 from knockpwd.Crypto import Crypto
 from knockpwd.KnockRequest import KnockRequest
 
 class KnockClient:
+    """A simple Client which is able to send KnockRequests
+    to the GateKeeper over UDP."""
 
     def __init__(self, socket_type, address_port_pair):
         self.socket_type = socket_type
@@ -11,13 +12,17 @@ class KnockClient:
 
     @staticmethod
     def create_knock_request(visit_duration):
+        """Create message payload for a KnockRequest."""
         return KnockRequest.create(visit_duration).to_raw()
 
     @staticmethod
     def create_encrypted_knock_request(visit_duration, key):
+        """Create encrypted message for a KnockRequest."""
         return Crypto.encrypt(KnockClient.create_knock_request(visit_duration), key)
 
     def knock(self, visit_duration, key):
-        request = KnockClient.create_encrypted_knock_request(visit_duration)
-        with socket.socket(self.socket_type, self.SOCK_DGRAM) as s:
-            socket.sendto(request, self.address_port_pair)
+        """Send a knock request with the given key and
+        visit duration to a GateKeeper (knockpwd server)."""
+        request = KnockClient.create_encrypted_knock_request(visit_duration, key)
+        with socket.socket(self.socket_type, socket.SOCK_DGRAM) as sock:
+            sock.sendto(request, self.address_port_pair)
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,17 +1,21 @@
 import unittest
+import logging
+
 from tests.GateKeeperTest import GateKeeperTest
 from tests.TestCrypto import TestCrypto
 
-import logging
-log_format_console = "%(levelname)s: %(message)s"
-log_format_logfile = "%(asctime)s |  " + log_format_console
-logging.basicConfig(filename='unittest.log',
-                    level=logging.DEBUG,
-                    format=log_format_logfile )
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.WARNING)
-stream_handler.setFormatter(logging.Formatter(log_format_console))
-logging.getLogger().addHandler(stream_handler)
+def main():
+    """Runs the unit test. The output is collected in unittest.log"""
+    log_format_console = "%(levelname)s: %(message)s"
+    log_format_logfile = "%(asctime)s |  " + log_format_console
+    logging.basicConfig(filename='unittest.log',
+                        level=logging.DEBUG,
+                        format=log_format_logfile)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.WARNING)
+    stream_handler.setFormatter(logging.Formatter(log_format_console))
+    logging.getLogger().addHandler(stream_handler)
+    unittest.main()
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
